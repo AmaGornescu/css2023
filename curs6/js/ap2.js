7412c98c6dc4055e8ba03071f885f9ac655862ba
Ana <ana@ana.ro>
2023-02-16 20:52:59 +0200

    curs6

---
+++ curs6/js/ap2.js
@@ -0,0 +1,9 @@
+// 1.definim o functie de tip lamda si sageata
+let adunare = (a,b) =>{
+    return a + b;
+}
+
+// 2. apelam functia
+console.log("Suma dintre 24 si 46 este", adunare(24, 46));
+
+// 3. In terminal rulam 
---
+++ curs6/js/main.js
@@ -0,0 +1,7 @@
+import Book, {citeste} from './book.js';
+
+// import * from "demo.js"
+
+let carte = new Book ('Poezii', 'Mihai Eminescu', 250);
+console.log(carte.toString());
+console.log(citeste());
